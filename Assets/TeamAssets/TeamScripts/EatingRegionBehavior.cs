8058ead8ec8fb95ca0b3acf3e6a02a1bed4c6ea9
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-06-19 23:33:47 -0400

    eating region works

---
+++ Assets/TeamAssets/TeamScripts/EatingRegionBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatingRegionBehavior : MonoBehaviour {
+
+    public GameObject gameStateObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On trigger enter");
+        // perhaps raw strings like "Prey" should belong to some class
+        // and called like other.tag == TeamTags.Prey
+        if (other.tag == "Prey") {
+            GameObject prey = other.gameObject;
+            gameStateObject.GetComponent<GameState>().eatSomething(prey);
+            Destroy(prey);
+        }
+    }
+}
