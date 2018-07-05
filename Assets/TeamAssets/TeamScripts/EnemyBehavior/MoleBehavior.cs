0aa61ac64f489e54f80fc0983efdd36274faf8cf
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-07-05 14:44:35 -0400

    mole raise and lower

---
+++ Assets/TeamAssets/TeamScripts/EnemyBehavior/MoleBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleBehavior : MonoBehaviour {
+    public enum MoleState { Rest, Raise, Lower }
+    public MoleState moleState = MoleState.Raise;
+    public float verticalSpeed = 0.4f;
+    public float timeInterval = 3f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(MoleMove());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (moleState)
+        {
+            case MoleState.Rest:
+                break;
+            case MoleState.Raise:
+                transform.position = transform.position + (new Vector3(0, verticalSpeed, 0) * Time.deltaTime);
+                break;
+            case MoleState.Lower:
+                transform.position = transform.position + (new Vector3(0, -verticalSpeed, 0) * Time.deltaTime);
+                break;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    IEnumerator MoleMove()
+    {
+        for (int i = 0; true; i = (i + 1) % 3)
+        {
+            moleState = (MoleState) (i + 1);
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+    }
+}
