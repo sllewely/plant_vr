4c27fa54b90385810bc318b42522dd80c69e42f1
larahelm <lara.helm@gmail.com>
2018-07-13 22:59:02 -0400

    if grabbing object only close partway and snap object location to middle of hand

--- Assets/TeamAssets/TeamScripts/Grab.cs
+++ Assets/TeamAssets/TeamScripts/Grab.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Grab : MonoBehaviour {
     public SteamVR_TrackedObject trackedObject;
     public SteamVR_Controller.Device device;
+    public bool isHolding = false;
     public float throwSpeed = 2.0f;
+    public GameObject grabSpot;
     // Use this for initialization
     void Start () {
         trackedObject = GetComponent<SteamVR_TrackedObject>();
@@ -24,12 +26,16 @@ public class Grab : MonoBehaviour {
             {
                 col.gameObject.GetComponent<Rigidbody>().isKinematic = true;
                 col.gameObject.transform.SetParent(gameObject.transform);
+                Collider boxcol = this.GetComponent<BoxCollider>();
+                col.gameObject.transform.position = grabSpot.transform.position;
+                isHolding = true;
             }
             if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
             {
                 col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
                 col.gameObject.transform.SetParent(null);
                 TossObject(col.attachedRigidbody);
+                isHolding = false;
             }
         }
     }
