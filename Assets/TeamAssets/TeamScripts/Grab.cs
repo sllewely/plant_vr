912ce91748058ede7a353cc73c8f252c57ffb1f7
larahelm <lara.helm@gmail.com>
2018-07-13 21:52:31 -0400

    Grab

---
+++ Assets/TeamAssets/TeamScripts/Grab.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObject;
+    public SteamVR_Controller.Device device;
+    public float throwSpeed = 2.0f;
+    // Use this for initialization
+    void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Prey")
+        {
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                col.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                col.gameObject.transform.SetParent(gameObject.transform);
+            }
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                col.gameObject.transform.SetParent(null);
+                TossObject(col.attachedRigidbody);
+            }
+        }
+    }
+    public void TossObject(Rigidbody rigidBody)
+    {
+        rigidBody.velocity = device.velocity * throwSpeed;
+        rigidBody.angularVelocity = device.angularVelocity;
+    }
+}
