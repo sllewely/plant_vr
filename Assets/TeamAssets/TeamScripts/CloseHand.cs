2304deb7f2808df5dbdcac9dba7dc2a9efb341f1
Lara <lara.helm@gmail.com>
2018-07-17 21:54:39 -0400

    scripted grip animation on arrow keys

--- Assets/TeamAssets/TeamScripts/CloseHand.cs
+++ Assets/TeamAssets/TeamScripts/CloseHand.cs
@@ -8,7 +8,7 @@ public class CloseHand : MonoBehaviour
     public GameObject leftSide;
     public GameObject rightSide;
     float smooth = 5.0f;
-    float tiltAngle = 60.0f;
+    float tiltAngle = 45.0f;
 
     void Update()
     {
@@ -16,10 +16,10 @@ public class CloseHand : MonoBehaviour
         //float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
         float tiltAroundY = Input.GetAxis("Vertical") * tiltAngle;
 
-        Quaternion target = Quaternion.Euler(0, tiltAroundY, 0);
-
+        Quaternion rtarget = Quaternion.Euler(0, tiltAroundY, 0);
+        Quaternion ltarget = Quaternion.Euler(0, -tiltAroundY, 0);
         // Dampen towards the target rotation
-        leftSide.transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-        rightSide.transform.rotation = Quaternion.Slerp(Quaternion.Inverse(transform.rotation), target, Time.deltaTime * smooth);
+        leftSide.transform.rotation = Quaternion.Slerp(leftSide.transform.rotation, rtarget, Time.deltaTime * smooth);
+        rightSide.transform.rotation = Quaternion.Slerp(rightSide.transform.rotation, ltarget, Time.deltaTime * smooth);
     }
 }
