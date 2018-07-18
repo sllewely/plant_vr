871aed1bb9212fc2231fd0da83fe7317cc0a3d4e
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-07-17 21:54:17 -0400

    beetle slides forward on a curve

--- Assets/TeamAssets/TeamScripts/EnemyBehavior/DartingBeetleBehavior.cs
+++ Assets/TeamAssets/TeamScripts/EnemyBehavior/DartingBeetleBehavior.cs
@@ -14,31 +14,71 @@ public class DartingBeetleBehavior : MonoBehaviour {
     bool dartRight;
     Quaternion rotateRight = Quaternion.Euler(new Vector3(0, 50, 0));
     Quaternion rotateLeft = Quaternion.Euler(new Vector3(0, -50, 0));
-    Quaternion from;
-    Quaternion to;
+    Vector3 fromPos;
+    Vector3 toPos;
+    Quaternion fromRot;
+    Quaternion toRot;
 
     // Use this for initialization
     void Start () {
         dart = false;
         dartRight = true;
         timeCount = 0;
-        from = rotateLeft;
-        to = rotateRight;
+        fromRot = rotateLeft;
+        toRot = rotateRight;
+        fromPos = transform.position;
+        toPos = Vector3.forward * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Rotate();
-	}
+        timeCount = timeCount + Time.deltaTime;
+        if (dart)
+        {
+            Dart();
+            if (timeCount > dartTime)
+            {
+                SetToRotate();
+            }
+        } else
+        {
+            Rotate();
+            if (timeCount > pauseTime)
+            {
+                SetToDart();
+            }
+        }
+    }
 
     void FixedUpdate()
     {
 
     }
 
+    void Dart()
+    {
+        transform.position = Vector3.Slerp(fromPos, toPos, timeCount / dartTime);
+    }
+
     void Rotate()
     {
-        transform.rotation = Quaternion.Slerp(from, to, timeCount/pauseTime);
-        timeCount = timeCount + Time.deltaTime;
+        transform.rotation = Quaternion.Slerp(fromRot, toRot, timeCount/pauseTime);
+    }
+
+    void SetToDart()
+    {
+        dart = true;
+        fromPos = transform.position;
+        toPos = (transform.forward * speed);
+        timeCount = 0;
+    }
+
+    void SetToRotate()
+    {
+        dart = false;
+        dartRight = !dartRight;
+        fromRot = Quaternion.Euler(Vector3.forward);
+        toRot = dartRight ? rotateRight : rotateLeft;
+        timeCount = 0;
     }
 }
