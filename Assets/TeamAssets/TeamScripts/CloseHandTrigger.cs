839d1b71cd0d005453ba7f3d819a3793595cd645
larahelm <lara.helm@gmail.com>
2018-07-22 23:02:13 -0400

    two-part rotation complete: hand closing, following parent rotation

--- Assets/TeamAssets/TeamScripts/CloseHandTrigger.cs
+++ Assets/TeamAssets/TeamScripts/CloseHandTrigger.cs
@@ -22,33 +22,11 @@ public class CloseHandTrigger : MonoBehaviour
     void Update()
     {
         device = SteamVR_Controller.Input((int)trackedObject.index);
-        // Smoothly tilts a transform towards a target rotation.
-        //float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+
         float closeLevel = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Trigger).x * 45;
-        leftSide.transform.rotation = Quaternion.Euler(0, closeLevel, 0);
-        rightSide.transform.rotation = Quaternion.Euler(0, -closeLevel, 0);
-        //Quaternion rotation = Quaternion.identity;
-        //rotation.eulerAngles = new Vector3(0, closeLevel, 0);
 
-
-        //    leftSide.transform.eulerAngles = new Vector3(
-        //leftSide.transform.eulerAngles.x,
-        //leftSide.transform.eulerAngles.y - closeLevel,
-        //leftSide.transform.eulerAngles.z
-        //);
-
-        //    rightSide.transform.eulerAngles = new Vector3(
-        //rightSide.transform.eulerAngles.x,
-        //rightSide.transform.eulerAngles.y + closeLevel,
-        //rightSide.transform.eulerAngles.z
-        //);
-
-        //Quaternion rtarget = Quaternion.Euler(0, closeLevel, 0);
-        //Quaternion ltarget = Quaternion.Euler(0, -closeLevel, 0);
-        // Dampen towards the target rotation
-        //leftSide.transform.rotation = Quaternion.Slerp(leftSide.transform.rotation, rtarget, Time.deltaTime * smooth);
-        //rightSide.transform.rotation = Quaternion.Slerp(rightSide.transform.rotation, ltarget, Time.deltaTime * smooth);
-
+        leftSide.transform.localEulerAngles = new Vector3(0, closeLevel, 0);
+        rightSide.transform.localEulerAngles = new Vector3(0, -closeLevel, 0);
 
     }
 
