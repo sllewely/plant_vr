f9c7a254ca6cde02bce4f1fb18585072e6400f92
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-07-26 22:18:29 -0400

    very simple spawner is working

--- Assets/TeamAssets/TeamScripts/EnemyBehavior/EnemySpawner.cs
+++ Assets/TeamAssets/TeamScripts/EnemyBehavior/EnemySpawner.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
+    // Very simple enemy spawner
+
+    Vector3 startingPos;
+    public Vector3 startingRot;
+    public float timeInterval;
+
+    public GameObject enemy;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        StartCoroutine(Spawner());
+        startingPos = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator Spawner()
+    {
+        for ( ; ; )
+        {
+            Instantiate(enemy, startingPos, Quaternion.Euler(startingRot));
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+    }
 }
