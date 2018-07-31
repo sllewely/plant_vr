cd6739e85d1f8a796a5c6e32fe02fc0ad9819614
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-07-30 23:09:56 -0400

    flower deaths

--- Assets/TeamAssets/TeamScripts/GameManager.cs
+++ Assets/TeamAssets/TeamScripts/GameManager.cs
@@ -7,16 +7,53 @@ public class GameManager : MonoBehaviour {
 
     public GameObject gameMenu;
     public GameObject menuHand;
-	
-	// Update is called once per frame
-	void Update () {
+
+    int lives;
+    Vector3 offset = new Vector3(0, 0.5f, 0);
+
+    // Plants that represent lives
+    public GameObject[] plantLives;
+    public GameObject deadPlantPrefab;
+
+    private void Start()
+    {
+        lives = plantLives.Length;
+    }
+
+    // Update is called once per frame
+    void Update () {
         // TODO: input
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            bool isMenuActice = gameMenu.activeSelf;
-            gameMenu.SetActive(!isMenuActice);
-            menuHand.SetActive(!isMenuActice);
+            LoseLife();   
         }
 		
 	}
+
+    void LoseLife()
+    {
+        if (IsDead())
+        {
+            Debug.LogWarning("Already dead! Lives: " + lives);
+            return;
+        }
+
+        lives--;
+        GameObject plantToKill = plantLives[lives];
+        Transform plantPosition = plantToKill.transform;
+        Instantiate(deadPlantPrefab, plantPosition.position + offset, plantPosition.rotation);
+        Destroy(plantToKill);
+    }
+
+    bool IsDead()
+    {
+        return lives <= 0;
+    }
+
+    void ToggleMenu()
+    {
+        bool isMenuActice = gameMenu.activeSelf;
+        gameMenu.SetActive(!isMenuActice);
+        menuHand.SetActive(!isMenuActice);
+    }
 }
