60d98e245922c3599dc3645cdcf13744d235748a
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-10-13 23:48:01 -0400

    score drains but does not drop below 0

--- Assets/TeamAssets/TeamScripts/GameState.cs
+++ Assets/TeamAssets/TeamScripts/GameState.cs
@@ -7,8 +7,12 @@ public class GameState : MonoBehaviour {
     int thingsEatenCounter = 0;
     public int startScore;
     int score;
+    public int scoreDrain;
+    public int timeBetweenDrain;
+    
     public int targetScore;
     int health = 3;
+    
 
     private GameObject feedNeedle;
 
@@ -17,6 +21,7 @@ public class GameState : MonoBehaviour {
         score = startScore;
         feedNeedle = GameObject.Find("Feed_meter/Needle");
         RotateNeedle();
+        StartCoroutine(DrainScore());
     }
 
     public void EatSomething(GameObject theThing)
@@ -43,9 +48,22 @@ public class GameState : MonoBehaviour {
 
     private void RotateNeedle()
     {
-        Debug.Log("Setting rot for " + feedNeedle.name);
-//        var currentRot = feedNeedle.transform.rotation;
         var z = score / (float)targetScore * 180;
         feedNeedle.transform.localRotation = Quaternion.Euler(0, 0, z);
+    }
+    
+    // Steadily drains the feed meter
+    private IEnumerator DrainScore()
+    {
+        while (true)
+        {
+            if (score - scoreDrain >= 0)
+            {
+                score -= scoreDrain;
+                RotateNeedle();
+            }
+            
+            yield return new WaitForSeconds(timeBetweenDrain);
+        }
     }
 }
